492194e55f6dfa46ecb2f809c6c586a3743f9428
AbdYildiz <abdullah-yildiz61@hotmail.com>
2022-10-25 15:43:34 +0300

    learning MNIST

--- Uygulama5.py
+++ Uygulama5.py
@@ -1,18 +1,19 @@
 # Gerekli kutuphanelerin eklenmesi
 import pandas as pd
-import SimpSOM as sps
+from sklearn_som.som import SOM as som
 from sklearn.cluster import KMeans
 import numpy as np
 
 veri = pd.read_csv("airline-safety.csv")
-X = veri.drop("airline", "avail_seat_km_per_week", axis=1)
+X = veri.drop("airline", axis=1)
+X = X.drop("avail_seat_km_per_week", axis=1)
 # Agin olusturulmasi
-net = sps.somNet(20, 20, X.values, PBC = True)
+net = som.__init__(self=X, m=20, n=20, dim=X.values)
 # Agin egitilmesi
-net.train(0.01, 10000)
+net.fit(0.01, 10000)
 # Veri noktalarinin 2 boyutlu bir haritaya gomulmesi ve kumelemenin yapilmasi
 hrt = np.array((net.project(X.values)))
-kmeans = KMeans(n_clusters = 3, max_iter = 300, random_state = 0)
+kmeans = KMeans(n_clusters=3, max_iter=300, random_state=0)
 # Kumeleme sonuclarinin gosterilmesi
 y_kmeans = kmeans.fit_predict(hrt)
 # Kumelerin etiketlerinin belirlenmesi
--- deneme.py
+++ deneme.py
@@ -37,3 +37,43 @@ plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 #%%
+
+from keras.datasets import mnist
+from tensorflow import keras
+from keras import layers
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+model = keras.Sequential([
+    layers.Dense(512, activation="relu"),
+    layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="rmsprop",
+             loss="sparse_categorical_crossentropy",
+             metrics=["accuracy"])
+
+train_images = train_images.reshape((60000, 28*28))
+train_images = train_images.astype("float32") / 255
+test_images = test_images.reshape((10000, 28*28))
+test_images = test_images.astype("float32") / 255
+
+model.fit(train_images, train_labels, epochs=5, batch_size=128)
+
+test_digits = test_images[0:10]
+predictions = model.predict(test_digits)
+predictions[0]
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"test_acc : {test_acc}")
+
+#%%
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from tensorflow import keras
+from keras import layers
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+digit = train_images[4]  # >>> train_labels[4]
+plt.imshow(digit, cmap=plt.cm.binary)
+plt.show()
+
+my_slice = train_images[10:100]
+
+#%%
